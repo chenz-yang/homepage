62d70795df6cf4e96e469e809dda85cf26fb7073
Zhiyang Chen <hi.zhiyang.chen@gmail.com>
2026-07-19 20:55:55 +0200

    Make audio methods bulletproof with try-catch and bump version to v105

--- games/shoot_game/game.js
+++ games/shoot_game/game.js
@@ -3,7 +3,7 @@ import { Obstacle, Tumbleweed, GroundSpike } from './obstacle.js';
 import { audio } from './audio.js';
 import { TRANSLATIONS } from './translations.js';
 
-const GAME_VERSION = '104';
+const GAME_VERSION = '105';
 console.log(`Wild West Duel - Loaded version ${GAME_VERSION}`);
 
 class Game {
